979a5735b1c56f8fd1ed17550881ca3eb36ab07e
Ar1f007 <muhammadarif9416@yahoo.com>
2022-06-01 19:59:52 +0600

    navbar for large device design done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
 import { Navbar } from './components/shared';
+import { Home } from './pages';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -1,13 +1,38 @@
-import Nav from './style';
+import { Nav, Wrapper } from './style';
+import logo from '../../../assets/icon/Logo.svg';
+import { Link, NavLink, useLocation } from 'react-router-dom';
 
 export const Navbar = () => {
+  const location = useLocation();
+  const currentLocation = location.pathname;
+  const pathIsHome = currentLocation === '/';
+
   return (
-    <Nav>
-      <div>
-        <h1>Logo</h1>
-      </div>
-      <div>Something</div>
-      <div>Something</div>
-    </Nav>
+    <Wrapper pathIsHome={pathIsHome}>
+      <Nav>
+        <Link to="/">
+          <img src={logo} alt="Logo - Luxury Living" />
+        </Link>
+
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about-us">About us</NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+          <li>
+            <NavLink to="/projects">Projects</NavLink>
+          </li>
+        </ul>
+        <Link to="/login" className="action">
+          Login
+        </Link>
+      </Nav>
+    </Wrapper>
   );
 };
